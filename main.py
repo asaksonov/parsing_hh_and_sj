b63cfa8a770a39596920e6f2dc1cb79922389678
Andrey Saksonov <asaksonov@gmail.com>
2023-09-11 23:31:45 +0300

    Реализованна логика

--- main.py
+++ main.py
@@ -1,6 +1,74 @@
-from src.classes import HeadHunter, SuperJob
+from src.classes import SuperJob, Connector, HeadHunter
+from src.utils import filter_by_keyword, filter_by_platform
+
 
 def main():
-    pass
+    hh_vacancies_json = []  # Отдельный список для вакансий с HeadHunter
+    sj_vacancies_json = []  # Отдельный список для вакансий с SuperJob
+
+    keyword = input('Введите ключевое слово для поиска вакансий: ')
+    hh = HeadHunter(keyword)
+    sj = SuperJob(keyword)
+
+    for api in (sj, hh):
+        api.get_vacancies(pages_count=5)
+        if isinstance(api, HeadHunter):
+            hh_vacancies_json.extend(api.get_formatted_vacancies())
+        elif isinstance(api, SuperJob):
+            sj_vacancies_json.extend(api.get_formatted_vacancies())
+
+    # Объединяем вакансии из обоих источников
+    vacancies_json = hh_vacancies_json + sj_vacancies_json
+
+    connector = Connector(keyword=keyword, vacancies_json=vacancies_json)
+
+    vacancies = connector.select()
+    # Цикл работы функцкий для пользователя
+    while True:
+        command = input(
+            "1 - Вывести список вакансий; \n"
+            "2 - Поиск по ключевому слову; \n"
+            "3 - ТОП вакансий по зарплате; \n"
+            "4 - Фильтр по платформе(HH or SuperJob); \n"
+            ">>> \n"
+            "exit - для выхода"
+        )
+        if command.lower() == 'exit':
+            break
+        elif command == "1":
+            vacancies = connector.select()
+        elif command == "2":
+            keyword = input("Введите ключевое слово для фильтрации: ")
+            vacancies = filter_by_keyword(vacancies, keyword)
+            if not vacancies:
+                print("Нет вакансий, соответствующих ключевому слову.")
+            else:
+                for vacancy in vacancies:
+                    print(vacancy, end='\n')
+        elif command == "3":
+            try:
+                n = int(input("Введите количество вакансий для отображения: "))
+                if n > 0:
+                    top_n_highest_paid = sorted(
+                        [v for v in vacancies if v.salary_to is not None],
+                        key=lambda x: x.salary_to,
+                        reverse=True)[:n]
+                    if not top_n_highest_paid:
+                        print("Нет вакансий с указанной зарплатой.")
+                    else:
+                        print(f"Топ {n} вакансий с самой крупной зарплатой:")
+                        for vacancy in top_n_highest_paid:
+                            print(vacancy, end='\n')
+                else:
+                    print("Введите положительное число.")
+            except ValueError:
+                print("Введите корректное число.")
+        elif command == "4":
+            platform = input("Введите платформу (hh или superjob) для фильтрации: ")
+            vacancies = filter_by_platform(vacancies, platform)
+            for vacancy in vacancies:
+                print(vacancy, end='\n')
 
 
+if __name__ == "__main__":
+    main()
--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,49 @@
-from ast import parse
-from configparser import ParsingError
-
 import requests
+import xml.etree.ElementTree as ET
+from src.exceptions import ParsingError
 
 
 def get_currencies():
-    responce = requests.get("http://www.cbr.ru/scripts/XML_daily.asp")
-
+    """
+    Функция парсинга курса валюты с сайта ЦБ
+    """
     try:
-        if responce.status_code != 200:
-            raise ParsingError(f"Ошибка получения курса валюты! Статус {responce.status_code}")
-        currencies = parse(responce.content)
+        response = requests.get("https://www.cbr.ru/scripts/XML_daily.asp")
+        if response.status_code != 200:
+            raise ParsingError(f"Ошибка получения курса валют! Статус:{response.status_code}")
+
+        root = ET.fromstring(response.content)
         formatted_currencies = {}
-        for currency in currencies["ValCurs"]["Valute"]:
-            value = float(currency["Value"].replace(",", "."))
-            nominal = float(currency["Nominal"])
-            formatted_currencies[currency["CharCode"]] = value / nominal
-        formatted_currencies["RUR"] = 1
+        for valute in root.findall('Valute'):
+            char_code = valute.find('CharCode').text
+            value = float(valute.find('Value').text.replace(',', '.'))
+            nominal = int(valute.find('Nominal').text)
+            rate = value / nominal
+            formatted_currencies[char_code] = rate
+
+        formatted_currencies['RUB'] = 1.0
         return formatted_currencies
     except ParsingError as error:
         print(error)
 
-cer = get_currencies()
-print(cer)
+
+def filter_by_keyword(vacancies, keyword):
+    """
+    Функция поиска по введенному пользователю слову
+    """
+    filtered_vacancies = []
+    keyword = keyword.lower()
+
+    for vacancy in vacancies:
+        if keyword in vacancy.title.lower() or keyword in vacancy.employer.lower():
+            filtered_vacancies.append(vacancy)
+
+    return filtered_vacancies
+
+
+def filter_by_platform(vacancies, platform):
+    """
+    Функция фильтрации вакансий по платформе
+    """
+    return [vacancy for vacancy in vacancies if vacancy.api.lower() == platform.lower()]
+
